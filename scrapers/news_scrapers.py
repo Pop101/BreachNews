51733e760a48ba2af5adfa950dfddd88ae23d0e6
crawlin <crawlin@uw.edu>
2024-10-12 15:34:22 -0700

    added cnn scraper

--- scrapers/news_scrapers.py
+++ scrapers/news_scrapers.py
@@ -28,8 +28,8 @@ class NewsScraper:
         """
         if news_company.lower() == "new york times":
             return self._scrape_new_york_times(url)
-        elif news_company.lower() == "another news company":
-            return self._scrape_another_news_company(url)
+        elif news_company.lower() == "cnn":
+            return self._scrape_cnn(url)
         # Add more elif cases for other news companies here
         else:
             raise ValueError(f"No scraping method defined for {news_company}")
@@ -72,6 +72,47 @@ class NewsScraper:
             driver.quit()  # Close the browser when done
 
         return article_text, full_html
+    
+
+    def _scrape_cnn(self, url):
+        """
+        Private method to scrape the CNN article given its URL.
+
+        Args:
+            url (str): The URL of the CNN article to scrape.
+
+        Returns:
+            tuple: A tuple containing:
+                - str: The cleaned text extracted from the article.
+                - str: The full HTML content of the article.
+        """
+        driver = webdriver.Chrome(options=self.chrome_options)
+        article_text = ""
+        full_html = ""
+        try:
+            driver.get(url)
+            wait = WebDriverWait(driver, 10)
+
+            full_html = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "storyTD")))
+
+            elements = full_html.find_elements(
+                By.XPATH, ".//p | .//*[contains(@class, 'inStoryHeading')]"
+            )
+
+            full_html = full_html.get_attribute('outerHTML')
+
+            article_html = ""
+            for element in elements:
+                article_html += element.get_attribute('outerHTML')
+
+            article_text = self.remove_html_tags(article_html)
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            driver.quit()  # Close the browser when done
+
+        return article_text, full_html
 
     @staticmethod
     def remove_html_tags(html):
@@ -83,7 +124,12 @@ class NewsScraper:
         return cleaned_text
 
 # Example usage:
+# scraper = NewsScraper()
+# url = "http://www.nytimes.com/2007/01/01/world/middleeast/01sunnis.html"
+# article_text, full_html = scraper.scrape(url, "New York Times")
+# print(article_text)
+
 scraper = NewsScraper()
-url = "http://www.nytimes.com/2007/01/01/world/middleeast/01sunnis.html"
-article_text, full_html = scraper.scrape(url, "New York Times")
+url = "http://money.cnn.com/2007/01/03/autos/new_chrysler_minivans/index.htm?cnn=yes"
+article_text, full_html = scraper.scrape(url, "CNN")
 print(article_text)
